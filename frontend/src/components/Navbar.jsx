4fd37a27931dd8740f7fde44717863581b4303e3
NatyMulugetaAlemu <metasebiazeleke@gmail.com>
2026-07-28 21:21:02 -0900

    refactor: clean up Navbar component and remove unused assets

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -8,12 +8,7 @@ const Navbar = () => {
         MyStore
       </div>
 
-      <ul className="nav-links">
-        <li><a href="/">Home</a></li>
-        <li><a href="/products">Products</a></li>
-        <li><a href="/about">About</a></li>
-        <li><a href="/contact">Contact</a></li>
-      </ul>
+     
 
       <div className="nav-actions">
         <button className="login-btn">

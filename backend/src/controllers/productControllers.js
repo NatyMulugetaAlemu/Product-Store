a7e5ad104e39944021b608a72d21937230d0acaa
NatyMulugetaAlemu <metasebiazeleke@gmail.com>
2026-07-28 18:32:13 -0900

    fix: enhance error handling and response messages in product controller functions

--- backend/src/controllers/productControllers.js
+++ backend/src/controllers/productControllers.js
@@ -6,11 +6,13 @@ export const getProducts = async (req, res) => {
     SELECT * FROM products
     ORDER BY created_at DESC 
     `
-        console.log(products)
-        res.status(200).json({ sucess: true, data: products })
+        if (products.length === 0) {
+            return res.status(404).json({ success: false, message: "Product Not Found" })
+        }
+        res.status(200).json({ success: true, data: products })
     } catch (error) {
         console.log("Error in getProducts Controller", error)
-        res.status(500).json({ sucess: false, message: error.message })
+        res.status(500).json({ success: false, message: error.message })
 
     }
 }
@@ -20,22 +22,22 @@ export const createProduct = async (req, res) => {
     try {
         const { name, price, image } = req.body
         if (!name || !price || !image) {
-            return res.status(400).json({ sucess: false, message: "All fields are required" })
+            return res.status(400).json({ success: false, message: "All fields are required" })
         }
 
         const newProduct = await sql`
-           INSERT INTO products(name,price,IMAGE)
-           VALUES (${name},${price},${imag})
+           INSERT INTO products(name,price,image)
+           VALUES (${name},${price},${image})
            RETURNING *
         `
 
         console.log(newProduct)
-        res.status(201).json({ sucess: true, data: newProduct[0] })
+        res.status(201).json({ success: true, data: newProduct[0] })
 
 
     } catch (error) {
         console.log("Error in createProduct Controller", error)
-        res.status(500).json({ sucess: false, message: error.message })
+        res.status(500).json({ success: false, message: error.message })
     }
 }
 
@@ -43,21 +45,58 @@ export const getProductById = async (req, res) => {
     try {
         const { id } = req.params
         const product = await sql`
-   SELECT * FROM products WHERE id={id}
+   SELECT * FROM products WHERE id=${id}
 `
 
-        res.status(200).json({ sucess: true, data: product[0] })
+        if (product.length === 0) {
+            return res.status(404).json({ success: false, message: "Product Not Found" })
+        }
+        res.status(200).json({ success: true, data: product[0] })
     } catch (error) {
-        console.log("Error in createProduct Controller", error)
-        res.status(500).json({ sucess: false, message: error.message })
+        console.log("Error in getProductById Controller", error)
+        res.status(500).json({ success: false, message: error.message })
     }
 
 }
 
 export const updateProduct = async (req, res) => {
+    try {
+        const { id } = req.params
+        const { name, price, image } = req.body
+
+        const updatedProduct = await sql`
+       UPDATE products 
+       SET name=${name},price=${price},image=${image})
+       WHERE id=${id}
+       RETURNING *
+     `
+
+        if (updatedProduct.length === 0) {
+            return res.status(404).json({ success: false, message: "Product Not Found" })
+        }
+        res.status(200).json({ success: true, data: updatedProduct[0] })
+    } catch (error) {
+        console.log("Error in updateProduct Controller", error)
+        res.status(500).json({ success: false, message: error.message })
+    }
 
 }
 
 export const deleteProduct = async (req, res) => {
+    try {
+        const { id } = req.params
 
+        const deletedProduct = await sql`
+      DELETE FROM products WHERE id=${id}  RETURNING *
+    `
+
+        if (deletedProduct.length === 0) {
+            return res.status(404).json({ success: false, message: "Product Not Found" })
+        }
+
+        res.status(200).json({ success: true, data: deletedProduct })
+    } catch (error) {
+        console.log("Error in deleteProduct Controller", error)
+        res.status(500).json({ success: false, message: error.message })
+    }
 }
